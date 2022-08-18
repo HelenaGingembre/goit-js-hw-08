05a1236a2ce3849060d75082fadc0f84e2ff7132
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-08-18 15:37:09 +0300

    TASK 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,47 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+    
+//5.Сохраняeм время воспроизведения в локальное хранилище.
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+const currentTimePlayerStorage = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY));
+console.log(currentTimePlayerStorage);
 
+// const currentTimeStorage = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || '';
+// console.log(currentTimeStorage);
+
+//3. Инициализируем плеер 
+const iframe = document.querySelector('#vimeo-player');    
+const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+    
+
+//4. отслеживаем событие timeupdate - обновление времени воспроизведения.
+const onTimeUpdate = function (data) {
+    localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(data));
+       // data is an object containing properties specific to that event
+};
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+//6.для того чтобы возобновить воспроизведение с сохраненной позиции при перезагрузке страницы
+player.setCurrentTime(currentTimePlayerStorage.seconds)
+    .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+// тест - получаем заголовок видео 
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,5 @@
+//Напиши скрипт который будет сохранять значения полей в локальное хранилище
+// когда пользователь что - то печатает.
+import throttle from 'lodash.throttle';
 
+const FEEDBACK_STORAGE_KEY = 'feedback-form-state';
